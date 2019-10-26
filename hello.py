2f872e745ede9bd7ac70feed011a6e441614aeb8
trungdipdo1 <trungdipdo7@gmail.com>
2019-10-26 13:18:44 +0700

    26/10

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@ print("hello")
 
 print("Trung is really best, his laptop is great with high performce calculating !!!")
 
+class op:
+    name: "Trung"
+    age: 19
+    majob: "studen"
+print(op.name + " " + op.age + "and is a" + op.majob)
