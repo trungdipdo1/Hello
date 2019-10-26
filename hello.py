84a54ce3499dc740026581087e0935468991166e
Mahouka <57008529+trungdipdo1@users.noreply.github.com>
2019-10-26 13:33:51 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,8 +2,5 @@ print("hello")
 
 print("Trung is really best, his laptop is great with high performce calculating !!!")
 
-class op:
-    name: "Trung"
-    age: 19
-    majob: "studen"
-print(op.name + " " + op.age + "and is a" + op.majob)
+print("How to leanrs good")
+print("I am in my branch")
